dcbe770d1e210e7ccc055a3423f1ced68e169c8a
Nishith08 <nishithsolanki14@gmail.com>
2025-05-09 11:49:25 +0530

    comments for better understanding of routes

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\CityDataController;
 
-Route::get('/update-cities-from-stores', [CityDataController::class, 'updateMissingDistricts']);
 
-Route::get('/stores', [StoreController::class, 'index']);
+Route::get('/stores', [StoreController::class, 'index']); 
 Route::post('/stores', [StoreController::class, 'store']);
 Route::get('/stores/search', [StoreController::class, 'search']);
 Route::post('/find-stores', [StoreController::class, 'findNearbyStores']);
+    //start of the data change routes
 Route::get('/update-districts', [StoreController::class, 'updateDistrictsFromPincode']);// updates the stores district in db based on pincode
-
+Route::get('/update-cities-from-stores', [CityDataController::class, 'updateMissingDistricts']);
+    //end of the data change routes
 /*
 |--------------------------------------------------------------------------
 | API Routes
